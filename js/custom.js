5c5c8d3645f20acddcc1befb16522da54505feef
jagdisharma <jagdishcsharma29@gmail.com>
2023-01-18 16:36:30 +0530

    chanegs

--- js/custom.js
+++ js/custom.js
@@ -374,7 +374,7 @@ $(".solar-cost-data .btn-main").click(function () {
     landing_page_url: window.location.href,
     user_agent: navigator.userAgent,
     monthy_bill: "",
-    tcpa_text: "By clicking the View My Savings button, you authorize www.powersolarsavings.com and up to 4 solar partners to call you and send you pre-recorded messages and text messages at the number you entered above, using an autodialer, with offers about their products or services, even if your phone number is on any national or state Do Not Call list. Message and data rates may apply. Your consent here is not based on a condition of purchase",
+    tcpa_text: "By clicking the View My Savings button, you authorize www.wisesolarenergy.com and up to 4 solar partners to call you and send you pre-recorded messages and text messages at the number you entered above, using an autodialer, with offers about their products or services, even if your phone number is on any national or state Do Not Call list. Message and data rates may apply. Your consent here is not based on a condition of purchase.",
     tcpa_optin: "Yes",
     utility_provider: "",
     project_timeframe: "Immediate",
